502c1c4fd09b5429d4f7e298fd4838e2edb6471a
Tufayel-Ahmed <tufayelahmed.edu@gmail.com>
2022-11-23 13:36:10 +0600

    Dot Net Core 6 Web API Versioning

---
+++ APIVersioning/APIVersioning/Controllers/UserV2Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIVersioning.Controllers
+{
+    //Route for query string parameter versioning
+    [Route("api/user/[action]")]
+    //Route for URI versioning
+    //[Route("api/v{version: apiVersion}/user/[action]")]
+    [ApiController]
+    [ApiVersion("2")]
+    public class UserV2Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetInfo()
+        {
+            return Ok("User v2 controller");
+        }
+    }
+}
